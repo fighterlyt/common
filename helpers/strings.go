e0f9568f11f482fbd17d1abc6fd2063e1ae3142f
figherlyt <fighter.lyt@gmail.com>
2021-08-25 17:37:30 +0400

    [helpers] string.go 添加IsStringEmpty 方法，判断是否字符串为空白(包括空格)

--- helpers/strings.go
+++ helpers/strings.go
@@ -12,12 +12,29 @@ func ContainsSub(data string, candidates ...string) bool {
 	return false
 }
 
-func Contains(path string, excludes ...string) bool {
-	for _, exclude := range excludes {
-		if exclude == path {
+/*Contains 是否保存，判断数组中是否存在某个字符串
+参数:
+*	value     	string   	是否存在的值
+*	candidates	...string	数组
+返回值:
+*	bool      	bool     	返回值1
+*/
+func Contains(value string, candidates ...string) bool {
+	for _, candidate := range candidates {
+		if value == candidate {
 			return true
 		}
 	}
 
 	return false
+}
+
+/*IsStringEmpty 判断字符串是否为空
+参数:
+*	str 	string	参数1
+返回值:
+*	bool	bool  	返回值1
+*/
+func IsStringEmpty(str string) bool {
+	return strings.TrimSpace(str) == ``
 }
