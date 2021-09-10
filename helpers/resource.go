a92c2d3da9b653dc26b184add4d4c7652379ff23
figherlyt <fighter.lyt@gmail.com>
2021-09-10 22:15:42 +0400

    [helpers] BaseResource 添加了一个用于测试的gin

--- helpers/resource.go
+++ helpers/resource.go
@@ -13,22 +13,25 @@ type BaseResource struct {
 	IRoutes     gin.IRoutes   // gin
 	RedisClient *redis.Client // redis 客户端
 	Logger      log.Logger    // 日志器
+	testIRoutes gin.IRoutes   // 用于测试或者模拟的gin
 }
 
 /*NewBaseResource 新建基础服务
 参数:
 *	db           	*gorm.DB     	数据库
 *	iRoutes      	gin.IRoutes  	gin
+*   testIRoutes     gin.IRoutes     测试gin
 *	redisClient  	*redis.Client   redis客户端
 *	logger       	log.Logger      日志器
 返回值:
 *	*BaseResource	*BaseResource	基础服务
 */
-func NewBaseResource(db *gorm.DB, iRoutes gin.IRoutes, redisClient *redis.Client, logger log.Logger) *BaseResource {
+func NewBaseResource(db *gorm.DB, iRoutes, testIRoutes gin.IRoutes, redisClient *redis.Client, logger log.Logger) *BaseResource {
 	return &BaseResource{
 		DB:          db,
 		IRoutes:     iRoutes,
 		RedisClient: redisClient,
 		Logger:      logger,
+		testIRoutes: testIRoutes,
 	}
 }
