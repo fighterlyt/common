d27b9d58d8ed7fe6a6875c25af60d920d1ee76dc
wuliangyou <976418217@qq.com>
2022-01-20 16:57:06 +0400

    修复mysql语句错误

--- summaryextend/client.go
+++ summaryextend/client.go
@@ -343,7 +343,7 @@ func (m client) GetSummarySummary(ownerIDs []string, from, to int64) (record Sum
 func (m client) GetSummaryExclude(excludeOwnerID []string, from, to int64, selects ...string) (records []Summary, err error) {
 	query := m.db.Session(&gorm.Session{})
 	if len(excludeOwnerID) > 0 {
-		query = query.Where(`ownerID nin ?`, excludeOwnerID)
+		query = query.Where(`ownerID not in ?`, excludeOwnerID)
 	}
 
 	var (
