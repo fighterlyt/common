de793def78a9776421d45e8ca36cd2906eaa490b
wuliangyou <976418217@qq.com>
2022-01-21 20:09:08 +0400

    手续费余额公共服务

--- model/paywallet.go
+++ model/paywallet.go
@@ -11,14 +11,3 @@ type BalanceDetail struct {
 	USDTBalance decimal.Decimal `json:"usdt_balance"`
 	FeeBalance  decimal.Decimal `json:"fee_balance"`
 }
-
-// WalletArgument 出款钱包参数
-type WalletArgument struct {
-	Protocol  Protocol // 协议
-	Symbol    string   // 币种
-	WalletKey string   // 钱包业务参数key
-}
-
-func NewWalletArgument(protocol Protocol, symbol string, walletKey string) *WalletArgument {
-	return &WalletArgument{Protocol: protocol, Symbol: symbol, WalletKey: walletKey}
-}
--- payWallet/service.go
+++ payWallet/service.go
@@ -1,7 +1,7 @@
 package paywallet
 
 import (
-	"gitlab.com/nova_dubai/common/parameters"
+	"fmt"
 	"go.uber.org/zap"
 	"time"
 
@@ -17,23 +17,21 @@ type Service struct {
 	checkBalanceInterval time.Duration                                                                    // 查询余额间隔时间
 	logger               log.Logger                                                                       // 日志器
 	CheckBalanceFunc     func(protocol model.Protocol, address, currency string) (decimal.Decimal, error) // 查询余额的方法
-	parameterService     parameters.Service
-	tronArgument         model.WalletArgument
-	ethArgument          model.WalletArgument
+	getAddressFunc       func(protocol model.Protocol) (address string, symbol string, err error)         // 获取参数的方法
+	protocols            []model.Protocol
 	tronBalanceDetail    *model.BalanceDetail
 	ethBalanceDetail     *model.BalanceDetail
 }
 
-func NewService(checkBalanceInterval time.Duration, parameterService parameters.Service, tronArgument, ethArgument model.WalletArgument, CheckBalanceFunc func(protocol model.Protocol, address, currency string) (decimal.Decimal, error), logger log.Logger) (*Service, error) { // nolint:golint,lll
+func NewService(protocols []model.Protocol, checkBalanceInterval time.Duration, getAddressFunc func(protocol model.Protocol) (address string, symbol string, err error), CheckBalanceFunc func(protocol model.Protocol, address, currency string) (decimal.Decimal, error), logger log.Logger) (*Service, error) { // nolint:golint,lll
 	service := &Service{
+		protocols:            protocols,
 		checkBalanceInterval: checkBalanceInterval,
 		logger:               logger,
 		CheckBalanceFunc:     CheckBalanceFunc,
-		tronArgument:         tronArgument,
-		ethArgument:          ethArgument,
-		parameterService:     parameterService,
-		tronBalanceDetail:    &model.BalanceDetail{},
-		ethBalanceDetail:     &model.BalanceDetail{},
+		getAddressFunc:       getAddressFunc,
+		tronBalanceDetail:    &model.BalanceDetail{Protocol: model.Trc20},
+		ethBalanceDetail:     &model.BalanceDetail{Protocol: model.Erc20},
 	}
 
 	helpers.EnsureGo(logger, func() {
@@ -51,44 +49,63 @@ func (s *Service) start() {
 
 // 单次查询余额
 func (s *Service) singleCheck() {
-	if err := s.checkWalletBalance(s.tronArgument); err != nil {
-		s.logger.Error("查询钱包余额失败", zap.Any("参数", s.tronArgument), zap.String("错误", err.Error()))
-	}
+	for _, protocol := range s.protocols {
+		address, symbol, err := s.getAddressFunc(protocol)
+		if err != nil {
+			s.logger.Error("查询钱包余额失败", zap.Any("协议", protocol), zap.String("错误", err.Error()))
 
-	if err := s.checkWalletBalance(s.ethArgument); err != nil {
-		s.logger.Error("查询钱包余额失败", zap.Any("参数", s.tronArgument), zap.String("错误", err.Error()))
+			continue
+		}
+
+		if err = s.checkWalletBalance(protocol, address, symbol); err != nil {
+			s.logger.Error("查询钱包余额失败", zap.Strings("协议/地址/币种", []string{protocol.String(), address, symbol}), zap.String("错误", err.Error()))
+		}
 	}
 }
 
-func (s *Service) checkWalletBalance(argument model.WalletArgument) (err error) {
-	result := model.BalanceDetail{Protocol: argument.Protocol}
+func (s *Service) checkWalletBalance(protocol model.Protocol, address, symbol string) (err error) {
+	var result *model.BalanceDetail
 
-	result.Address, err = s.parameterService.GetString(argument.WalletKey)
-	if err != nil {
-		return errors.Wrap(err, "查询业务参数失败")
+	switch protocol {
+	case model.Trc20:
+		result = s.tronBalanceDetail
+	case model.Erc20:
+		result = s.ethBalanceDetail
+	default:
+		return fmt.Errorf("不支持的协议[%s]", protocol)
 	}
 
+	result.Address = address
+
 	// 查询usdt余额
-	result.USDTBalance, err = s.CheckBalanceFunc(argument.Protocol, result.Address, argument.Symbol)
+	result.USDTBalance, err = s.CheckBalanceFunc(protocol, address, symbol)
 	if err != nil {
 		return errors.Wrap(err, "查询USDT余额失败")
 	}
 
 	// 查询手续费余额
 	feeSymbol := model.ETH
-	if argument.Protocol == model.Trc20 {
+	if protocol == model.Trc20 {
 		feeSymbol = model.TRX
 	}
 
-	result.FeeBalance, err = s.CheckBalanceFunc(argument.Protocol, result.Address, feeSymbol)
+	result.FeeBalance, err = s.CheckBalanceFunc(protocol, address, feeSymbol)
 	if err != nil {
-		return errors.Wrap(err, "查询USDT余额失败")
+		return errors.Wrap(err, "查询手续费余额失败")
 	}
 
 	return nil
 }
 
 // GetPayWalletDetails 查询钱包余额
-func (s Service) GetPayWalletDetails() (result []model.BalanceDetail, err error) {
+func (s Service) GetPayWalletDetails() (result []*model.BalanceDetail, err error) {
+	if s.tronBalanceDetail != nil {
+		result = append(result, s.tronBalanceDetail)
+	}
+
+	if s.ethBalanceDetail != nil {
+		result = append(result, s.ethBalanceDetail)
+	}
+
 	return result, nil
 }
