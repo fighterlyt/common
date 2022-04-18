1f0b60621b5fdc0633593de396fec0343f548f77
wuliangyou <976418217@qq.com>
2022-04-18 14:47:08 +0400

    含有复合表头的导出

--- helpers/export.go
+++ helpers/export.go
@@ -91,3 +91,81 @@ func charAdd(from byte, delta int) byte {
 
 	return from
 }
+
+// BuildXLSFileHaveSub 含有复合表头的导出
+func BuildXLSFileHaveSub(writer io.Writer, headers map[string]int, headerOrders []string, subHeaders map[string]int, subHeaderOrders map[string][]string, title string, records ...ExportRecord) error {
+	f := excelize.NewFile()
+
+	sheet1 := `Sheet1`
+	// Create a new sheet.
+	index := f.NewSheet(sheet1)
+	style, _ := f.NewStyle(&excelize.Style{Alignment: &excelize.Alignment{
+		Horizontal: "center",
+	}})
+
+	last := charAdd('A', len(headers)-1)
+	f.MergeCell(sheet1, `A1`, string(last)+`1`)
+
+	f.SetCellValue(sheet1, `A1`, title)
+
+	i := 0
+	for _, text := range headerOrders {
+
+		column := string(charAdd('A', i))
+		f.SetColStyle(sheet1, column, style)
+		f.SetColWidth(sheet1, column, column, float64(headers[text]))
+
+		f.SetCellValue(sheet1, column+"2", text)
+
+		var sub = subHeaderOrders[text]
+
+		if len(sub) == 0 {
+			// 没用子表头，上下合并
+			c := string(charAdd('A', i))
+
+			f.MergeCell(sheet1, c+"2", c+"3")
+
+			i++
+		} else {
+			startIndex := i
+
+			for _, subText := range sub {
+				column1 := string(charAdd('A', i))
+				f.SetColStyle(sheet1, column1, style)
+				f.SetColWidth(sheet1, column1, column1, float64(subHeaders[subText]))
+
+				f.SetCellValue(sheet1, column1+"3", subText)
+
+				i++
+			}
+
+			start := string(charAdd('A', startIndex)) + "2"
+			end := string(charAdd('A', startIndex+len(sub)-1)) + "2"
+
+			f.MergeCell(sheet1, start, end)
+		}
+	}
+
+	// Set active sheet of the workbook.
+	f.SetActiveSheet(index)
+	// Save spreadsheet by the given path.
+
+	var startColumn = 3
+
+	if subHeaderOrders != nil {
+		startColumn = 4
+	}
+
+	for i, record := range records {
+		for j, field := range record.GetExportFields() {
+			column := string(charAdd('A', j))
+			f.SetCellValue(sheet1, fmt.Sprintf(`%s%d`, column, i+startColumn), field)
+		}
+	}
+
+	if err := f.Write(writer); err != nil {
+		return errors.Wrap(err, `写入应答`)
+	}
+
+	return nil
+}
