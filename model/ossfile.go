620a0d4de78961e8687259f442c8c55c13ea4d5e
wuliangyou <976418217@qq.com>
2022-07-08 16:26:34 +0400

    oss路径

--- model/ossfile.go
+++ model/ossfile.go
@@ -1,12 +1,15 @@
 package model
 
 import (
+	"github.com/pkg/errors"
 	"strings"
 )
 
 var (
-	fromHost string // 待替换的域名
-	toHost   string // 替换的域名
+	fromHost      string // 待替换的域名
+	isSetFromHost bool   // 是否设置了待替换的域名
+	toHost        string // 替换的域名
+	isSetToHost   bool   // 是否设置了替换后的域名
 )
 
 /*SetOssFromHost 设置代替换域名
@@ -16,6 +19,7 @@ var (
 */
 func SetOssFromHost(from string) {
 	fromHost = from
+	isSetFromHost = true
 }
 
 /*SetOssToHost 设置替换后的域名
@@ -25,6 +29,7 @@ func SetOssFromHost(from string) {
 */
 func SetOssToHost(to string) {
 	toHost = to
+	isSetToHost = true
 }
 
 // OssFilePath 存储到oss的文件路径
@@ -37,6 +42,14 @@ type OssFilePath string
 *	error 	error 	错误
 */
 func (o OssFilePath) MarshalJSON() ([]byte, error) {
+	if !isSetFromHost {
+		return nil, errors.New("请先设置待替换域名")
+	}
+
+	if !isSetToHost {
+		return nil, errors.New("请先设置替换后的域名")
+	}
+
 	if fromHost == "" || toHost == "" { // 任意一个host没有设置都返回原来的路径
 		return []byte(o), nil
 	}
